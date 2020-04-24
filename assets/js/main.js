8186da543bb9aab5d48d41a84ca4fdf00385c98d
Leo Lawrence <rubiks5489@gmail.com>
2020-04-23 22:56:49 -0400

    Progress on the form
    Not done by any means, but closer than what it was before. All code is based off of a tutorial that still needs editing.

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,6 +3,7 @@
 * Template URL: https://bootstrapmade.com/maundy-free-coming-soon-bootstrap-theme/
 * Author: BootstrapMade.com
 * License: https://bootstrapmade.com/license/
+* Form made by Leo Lawrence
 */
 !(function($) {
   "use strict";
@@ -32,4 +33,37 @@
     return false;
   });
 
+  var form = $('.php-email-form'),
+      message = $('.form-control'),
+      form_data;
+  // Success function
+  function done_func(response) {
+      message.fadeIn().removeClass('alert-danger').addClass('alert-success');
+      message.text(response);
+      setTimeout(function () {
+          message.fadeOut();
+      }, 2000);
+      form.find('input:not([type="submit"]), textarea').val('');
+  }
+  // fail function
+  function fail_func(data) {
+      message.fadeIn().removeClass('alert-success').addClass('alert-success');
+      message.text(data.responseText);
+      setTimeout(function () {
+          message.fadeOut();
+      }, 2000);
+  }
+  
+  form.submit(function (e) {
+      e.preventDefault();
+      form_data = $(this).serialize();
+      $.ajax({
+          type: 'POST',
+          url: form.attr('action'),
+          data: form_data
+      })
+      .done(done_func)
+      .fail(fail_func);
+  });
+  
 })(jQuery);
